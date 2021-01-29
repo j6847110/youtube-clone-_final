0be4b8de30e985bf2bd8a97a1f5f575254e234d0
j6847110 <75517361+j6847110@users.noreply.github.com>
2021-01-29 16:59:40 +0900

    middleware

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("hi");
+import express from "express";
+const app = express();
+let PORT = 4000;
+
+const handleHome = (req, res) => res.send("hi from fuckin ");
+const handleProfile = (req, res) => res.send("you are on my profile");
+const handleBetween = (req, res, next) => {
+  console.log("hi I'm between");
+  next();
+};
+function handleListening() {
+  console.log(`You are listening on:http://localhost:${PORT}`);
+}
+app.use(handleBetween);
+app.get("/", handleHome);
+app.get("/profile", handleProfile);
+
+app.listen(PORT, handleListening);
