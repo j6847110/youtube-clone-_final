d3ea9ddc3a85d3d997035d5c7bcaeca648aece9a
j6847110 <75517361+j6847110@users.noreply.github.com>
2021-02-05 18:19:15 +0900

    2.23 done

---
+++ db.js
@@ -0,0 +1,41 @@
+
+export const videos = [
+{
+    id:324245,
+    title:'Video awesome',
+    description:'This is something',
+    views:24,
+    videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator : {
+        id:12132,
+        name:"Nam",
+        email:"nam@ja.com"
+    }
+},
+{
+    id:124545,
+    title:'Video Nice',
+    description:'This is something',
+    views:24,
+    videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator : {
+        id:12132,
+        name:"Nam",
+        email:"nam@ja.com"
+    }
+},
+{
+    id:987323,
+    title:'Video Hurray',
+    description:'This is something',
+    views:24,
+    videoFile:"https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator : {
+        id:12132,
+        name:"Nam",
+        email:"nam@ja.com"
+    }
+}
+
+
+]
--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,11 +1,14 @@
 import express from "express";
-import { join, login, logout } from "../controllers/userControllers";
+import { getJoin,postJoin, login, logout } from "../controllers/userControllers";
 import { home, search } from "../controllers/videoControllers";
 import routes from "../routes";
 
 const globalRouter = express.Router();
 globalRouter.get(routes.home, home);
-globalRouter.get(routes.join, join);
+
+globalRouter.get(routes.join, getJoin);
+globalRouter.post(routes.join, postJoin);
+
 globalRouter.get(routes.login, login);
 globalRouter.get(routes.logout, logout);
 globalRouter.get(routes.search, search);
